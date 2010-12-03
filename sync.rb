2dbfb973221a0b16345a1db35091f76a72efa60b
root <root@ip-10-228-138-112.eu-west-1.compute.internal>
2010-12-03 13:12:08 +0000

    First cut

---
+++ sync.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'sinatra'
+require 'json/pure'
+require 'git'
+require 'yaml'
+
+CONFIG = YAML::load_file("config.yml") unless defined? CONFIG
+
+get '/' do
+    'Nothing to see here'
+end
+
+post '/notify' do
+   payload = JSON.parse(params[:payload])
+   repo_dir = CONFIG["repo_dir"]
+   this_repo = repo_dir + "/" +  payload["repository"]["name"]
+   repo = Git.open(this_repo)
+   repo.pull
+end
